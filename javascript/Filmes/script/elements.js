f4bcb116a3512f9a9c75eab48eda15e15c952b78
Everon-auth <evertonauth@gmail.com>
2021-06-24 14:13:56 -0300

    estável

--- javascript/Filmes/script/elements.js
+++ javascript/Filmes/script/elements.js
@@ -1,5 +1,5 @@
 function menuadd(){
-    section.innerHTML = '<div><span>'+
+    section.innerHTML = '<div id="adicionar"><span id="main">'+
     '<button id="closeadd">X</button> </span>'+
     '<span><label class="labelform">Nome do Filme:</label>'+
     '<input type"text" id="filme" class="inpform">'+
@@ -13,11 +13,56 @@ function menuadd(){
     '<input type"text" id="osc" class="inpform">'+
     '<button id="send">Salvar</button>'
     '</span></div>'
+
+    menuaddestiliza()
 }
 
 function vizualizer(){
 
 }
 function list(){
+}
+
+function menuaddestiliza(){
+    /* Espaço do botao add */ 
+    let espaco = document.getElementById("main")
     
+    espaco.style.display="flex"
+    espaco.style.alignItems="flex-end"
+    espaco.style.backgroundColor="#efefef"
+
+    /* botão de salvar */
+    let btnsend = document.getElementById("send")
+
+    btnsend.style.marginTop="10px"
+    btnsend.style.borderRadius="7px"
+    btnsend.style.backgroundColor="rgb(125,180,43)"
+    btnsend.style.boxSizing="border-box"
+    btnsend.style.padding="20px"
+    //falta o hover
+
+    /* Botão de fechar */
+    let fechaadd = document.getElementById("closeadd")
+
+    fechaadd.style.fontSize="22px"
+    fechaadd.style.backgroundColor="none"
+    fechaadd.style.fontWeight="bolder"
+    fechaadd.style.border="none"
+    fechaadd.style.alignItems="center"
+    fechaadd.style.display="flex"
+    fechaadd.style.justifyContent="center"
+    fechaadd.style.width="35px"
+    fechaadd.style.height="35px"
+    //falta o hover
+
+    /* caixa do menu adicionar */
+    
+    let adicionar = document.getElementById("adicionar")
+    
+    adicionar.style.borderStyle="solid"
+    adicionar.style.borderWidth="1px"
+    adicionar.style.borderColor="tomato"
+    adicionar.style.boxSizing="border-box"
+    adicionar.style.padding="20px"
+
 }
--- javascript/Filmes/script/event.js
+++ javascript/Filmes/script/event.js
@@ -1,11 +1,10 @@
 var add = document.getElementById("add");
 var filmes = document.getElementById("btn-filmes");
-var main = document.getElementsByTagName("main");
 var padd = document.getElementById("padd");
 var section = document.getElementsByTagName("SECTION")[0]
 var tela = 0;
 
-
+section.innerHTML=""
 
 add.addEventListener("click", function(){ 
    
@@ -18,9 +17,48 @@ add.addEventListener("click", function(){
     })
     var send = document.getElementById("send")
     send.addEventListener("click", function(){
+        
+
+        if(filme.value!="" && year.value !="" && gen.value!="" && dur.value!="" && osc.value!=""){
         savedb();
+        section.innerHTML=""
+    }else{
+        if(filme.value == ""){
+            console.log(filme.value)
+            filme.style.borderColor="red"
+        }
+        if(year.value == ""){
+            console.log(year.value)
+            year.style.borderColor="red"
+        }
+        if(gen.value == ""){
+            console.log(gen.value)
+            gen.style.borderColor="red"
+        }
+        if(dur.value == ""){
+            console.log(dur.value)
+            dur.style.borderColor="red"
+        }
+        if(osc.value == ""){
+            console.log(osc.value)
+            osc.style.borderColor="red"
+        }
+    }
     })
 })
 filmes.addEventListener("click", function(){
-
+    section.innerHTML=""
+    
+    getdb()
+/*
+    for (let a = 1; a < localStorage.length+1; a++) {
+        var imgs = document.getElementById(a)
+    
+        imgs.addEventListener("click", function(){
+            localStorage.removeItem(a)
+        })
+        
+    }
+    */
 })
+
