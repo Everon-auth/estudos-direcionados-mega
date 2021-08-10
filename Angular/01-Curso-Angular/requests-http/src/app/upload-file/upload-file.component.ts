d3a729222782f2ca411d6080c7db839897dc2e28
Everon-auth <evertonauth@gmail.com>
2021-08-10 14:18:48 -0300

    video 147 min: 0

--- Angular/01-Curso-Angular/requests-http/src/app/upload-file/upload-file.component.ts
+++ Angular/01-Curso-Angular/requests-http/src/app/upload-file/upload-file.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UploadFileService } from './upload-file.service';
+import { filterResponse, uploadProgress } from '../shared/rxjs.operators';
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-upload-file',
   templateUrl: './upload-file.component.html',
@@ -7,12 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadFileComponent implements OnInit {
 
-  constructor() { }
+  files: Set<File> = new Set();
+  progress = 0;
+  progressDownload=0;
+
+  constructor(
+    private service: UploadFileService,
+  ) { }
 
   ngOnInit() {
   }
 
-  onChenge(event){
+  onChange(event){
     const selectedFiles = <FileList>event.srcElement.files
+    const fileNames=[];
+
+    for(let i =0; i < selectedFiles.length;i++){
+      this.files.add(selectedFiles[i])
+    }
+
+  }
+  onSubmit(){
+    this.progress = 0;
+    if(this.files && this.files.size >0){
+      this.service.upload(this.files,`${environment.BASE_URL}/upload`)
+      .pipe(
+        uploadProgress(progr => this.progress = progr),
+        filterResponse(),
+      ).subscribe(response => console.log(`Upload Conclude`));
+    }
+  }
+
+  onDownloadExcel(){
+    this.service.download(`${environment.BASE_URL}/downloadExcel`).subscribe((res:any) =>{
+      this.service.handleFile(res,'report.xlsx')
+    })
+  }
+  onDownloadPDF(){
+    this.service.download(`${environment.BASE_URL}/downloadPDF`).subscribe((res:any) =>{
+      this.service.handleFile(res,'report.pdf')
+    })
+
   }
 }
