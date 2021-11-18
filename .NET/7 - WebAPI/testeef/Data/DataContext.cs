3f539a483a140380dbd4b60f542ce81beb375d38
Everton-Auth <evertonauth@gmail.com>
2021-11-17 22:53:58 -0300

    criando web api com asp net core

---
+++ .NET/7 - WebAPI/testeef/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using testeef.Models;
+
+namespace testeef.Data {
+    public class DataContext : DbContext {
+
+        public DataContext( DbContextOptions<DataContext> options )
+            : base( options: options ) {
+        }
+
+        public DbSet<Product> products { get; set; }
+        public DbSet<Category> categories { get; set; }
+
+
+    }
+}
